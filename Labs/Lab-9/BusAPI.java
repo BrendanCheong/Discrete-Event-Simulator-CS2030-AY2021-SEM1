457506d6ac77c4cb28c8133c5fc9b82fc40fd15e
BrendanCheong <brendancej82@gmail.com>
2021-11-17 23:12:56 +0800

    small error handling

--- Labs/Lab-9/BusAPI.java
+++ Labs/Lab-9/BusAPI.java
@@ -6,9 +6,9 @@ import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletableFuture;
 
 /**
- * The BusAPI class interface with the Web API running at 
+ * The BusAPI class interface with the Web API running at
  * cs2030-bus-api.herokuapp.com to retrieve:
- * - bus services that serve a given bus stop 
+ * - bus services that serve a given bus stop
  * - bus stop visited by a bus service.
  *
  * @author: Brendan Cheong
@@ -19,11 +19,11 @@ class BusAPI {
     private static final int HTTP_RESPONSE_STATUS_OK = 200;
 
     /** URL to query for bus stops. */
-    private static final String BUS_SERVICE_API = 
+    private static final String BUS_SERVICE_API =
         "https://cs2030-bus-api.herokuapp.com/bus_services/";
 
     /** URL to query for bus services. */
-    private static final String BUS_STOP_API = 
+    private static final String BUS_STOP_API =
         "https://cs2030-bus-api.herokuapp.com/bus_stops/";
 
     /**
@@ -31,7 +31,7 @@ class BusAPI {
      * from the URL.  It returns an empty string and prints an
      * error message if the URL is invalid (not the best behavior).
      * @param url The URL to query
-     * @return The HTTP resposne body, or an empty string if the 
+     * @return The HTTP resposne body, or an empty string if the
      *     query fails.
      */
     private static CompletableFuture<String> httpGet(String url) {
@@ -41,22 +41,20 @@ class BusAPI {
             .uri(URI.create(url))
             .build();
         CompletableFuture<String> response;
+        response = client
+            .sendAsync(request, BodyHandlers.ofString())
+            .thenApply((resp) -> {
+                if (resp.statusCode() != HTTP_RESPONSE_STATUS_OK) {
+                    System.out.println(resp + " " + resp.statusCode());
+                    return "";
+                }
 
-        try {
-            response = client
-                .sendAsync(request, BodyHandlers.ofString())
-                .thenApply((resp) -> {
-                    if (resp.statusCode() != HTTP_RESPONSE_STATUS_OK) {
-                        System.out.println(resp + " " + resp.statusCode());
-                        return "";
-                    }
-
-                    return resp.body();
-                });
-            return response;
-        } catch (Exception e) {
-            throw new CompletionException(e);
-        }
+                return resp.body();
+            })
+            .exceptionally((e) -> {
+                throw new CompletionException(e);
+            });
+        return response;
     }
 
     /**
@@ -65,7 +63,7 @@ class BusAPI {
      * @return The string returned by the CS2030 BUS API service listing
      *     the bus stops that are serviced at this bus.  Return an empty
      *     string if something go wrong.
-     */ 
+     */
     public static CompletableFuture<String> getBusStopsServedBy(String serviceId) {
         return httpGet(BUS_SERVICE_API + serviceId);
     }
@@ -74,9 +72,9 @@ class BusAPI {
      * Return the string from CS2030 BUS API given a bus stop ID.
      * @param stopId Bus stop id
      * @return The string returned by the CS2030 BUS API service listing
-     *     the bus services that stopped at this bus stop; an empty 
+     *     the bus services that stopped at this bus stop; an empty
      *     string if the API query failed.
-     */ 
+     */
     public static CompletableFuture<String> getBusServicesAt(String stopId) {
         return httpGet(BUS_STOP_API + stopId);
     }
