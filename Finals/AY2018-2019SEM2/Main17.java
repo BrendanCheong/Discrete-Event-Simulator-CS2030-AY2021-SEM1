5d13666d64ef86082809a08d6a0242de2db2948e
BrendanCheong <brendancej82@gmail.com>
2021-11-24 21:01:15 +0800

    small modifications

---
+++ Finals/AY2018-2019SEM2/Main17.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main17 {
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            sc.close();
+            int len1 = String.valueOf(i).length();
+            int len2 = String.valueOf(j).length();
+            IntStream // continue the stream pipeline below
+                .rangeClosed(len1, len2)
+                .mapToObj((x) -> {
+                    int[] first = new int[] { 
+                        len1 >= x + 1
+                            ? Integer.parseInt(String.valueOf(i).substring(len1 - 1 - x, len1))
+                            : i,
+                        len2 >= x + 1
+                            ? Integer.parseInt(String.valueOf(j).substring(len2 - 1 - x, len2))
+                            : j,
+                            x
+                    };
+                    return first;
+                }).map((x) -> {
+                    int l = x[0] / (int) Math.pow(10, x[2]) % 10;
+                    int r = x[1] / (int) Math.pow(10, x[2]) % 10;
+                    int q = (x[0] + x[1]) / (int) Math.pow(10, x[2]) % 10;
+                    int c = (x[0] + x[1]) / (int) Math.pow(10, x[2]);
+                    return l + " + " + r + ((l + r) % 10 != (q % 10) 
+                        ? " + (1)" 
+                        : "") + " = " + q + (c >= 10 
+                            ? " carry 1" 
+                            : "");
+                }).filter((x) -> !x.equals("0 + 0 = 0"))
+                .forEach((line) -> System.out.println(line));
+    }
+}
--- Finals/AY2020-2021SEM1/oop-d.java
+++ Finals/AY2020-2021SEM1/oop-d.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
